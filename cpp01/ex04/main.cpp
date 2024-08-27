22115c5a85fafcf7a25b9950a44dbd05d2b6c641
didin0 <mehdi.abbadi.pro@gmail.com>
2024-08-27 18:02:40 +0200

    ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,32 +1,50 @@
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <fstream>
 
 int main(int argc, char **argv) {
-	if (argc == 3) {
-		// arguments
-		if (strlen(argv[1]) <= 0 || strlen(argv[2]) <= 0) {
-			std::cout << "You cannot provide empty arguments" << std::endl;
-			return 1;
+	// Arguments
+	if (argc != 4) {
+		std::cout << "Usage: <filename> <string1> <string2>" << std::endl;
+		return 1;
+	}
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	if (filename.empty() || s1.empty() || s2.empty()) {
+		std::cout << "You cannot provide empty arguments" << std::endl;
+		return 1;
+	}
+	if (s1 == s2) {
+		std::cout << "Please enter two different strings" << std::endl;
+		return 1;
+	}
+	// Open file to read
+	std::ifstream ifs(filename.c_str());
+	if (!ifs) {
+		std::cout << "Failed to open the file: " << filename << std::endl;
+		return 2;
+	}
+	// Create file to write
+	std::ofstream ofs((filename + ".replace").c_str());
+	if (!ofs) {
+		std::cout << "Failed to create the file: " << filename + ".replace" << std::endl;
+		return 3;
+	}
+	// Find and replace occurences than write
+	std::string line;
+	while (std::getline(ifs, line)) {
+		size_t pos;
+		while ((pos = line.find(s1)) != std::string::npos) {
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
 		}
-		else if (!strcmp(argv[1], argv[2])) {
-			std::cout << "Please, enter 2 differents arugments" << std::endl;
-			return 1;
-		}
-		// opening file
-		std::ifstream ifs;
-		ifs.open("test.txt", std::ifstream::in);
-		char c = ifs.get();
-		while (ifs.good()) {
-			std::cout << c;
-			c = ifs.get();
-		}
-
-		ifs.close();
-	}	
-	else
-		std::cout << "here"	<< std::endl;
+		ofs << line << std::endl;
+	}
+	// Closing streams
+	ifs.close();
+	ofs.close();
 
 	return 0;
 }
+
