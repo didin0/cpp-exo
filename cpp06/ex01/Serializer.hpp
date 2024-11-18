dd6b552d17ee18781780c93827c709466f0dd57b
didin0 <mehdi.abbadi.pro@gmail.com>
2024-11-18 14:22:33 +0100

    f

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,30 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <cstdint>
+
+
+struct Data {
+	int n;
+};
+
+class Serializer {
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+private:
+    // Canonical form
+    Serializer() {}
+    ~Serializer() {}
+    Serializer(const Serializer &copy);
+    Serializer& operator=(const Serializer &assign);
+};
+
+#endif
+
