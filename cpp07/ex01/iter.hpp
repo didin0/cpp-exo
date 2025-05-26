992b598c2e59ad89354742b30b30c6791b187d4b
didin0 <mehdi.abbadi.pro@gmail.com>
2025-05-26 15:58:55 +0200

    secure

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 template <typename T> void iter(T* array, size_t length, void (*func)(T&)) {
+    if (!array || length == 0 || !func) {
+        std::cerr << "Error: Invalid input to iter function." << std::endl;
+        return;
+    }
     for (size_t i = 0; i < length; i++) {  
         func(array[i]);
     }
